2a35cc345b8086ae0dcc7a4e9c80458051e5b310
luve12vz <samira.2001@hotmail.com>
2023-08-07 15:14:50 -0500

    cambios modelo vuelos

--- backend/models/vuelo.js
+++ backend/models/vuelo.js
@@ -1,4 +1,4 @@
-'use strict'
+'use strict';
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
@@ -11,8 +11,10 @@ var VueloSchema = Schema({
     required: true
   },
   precio: Number,
-  horaLlegada: Number, // Hora de llegada en formato de 24 horas (puedes ajustar si necesitas minutos)
-  horaSalida: Number, // Hora de salida en formato de 24 horas (puedes ajustar si necesitas minutos)
+  horaLlegada: Number, 
+  horaSalida: Number,
+  pasajerosMaximo: Number,  
+  pasajerosReserva: Number,  
 }, {
   collection: 'vuelos'
 });
