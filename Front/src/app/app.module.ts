e60818b0365f2ab88c965a9d56a6e2789a38a9ad
Chafiiiiii <kevin.casagualpa@epn.edu.ec>
2023-08-07 19:02:48 -0500

    conexion front

---
+++ Front/src/app/app.module.ts
@@ -0,0 +1,30 @@
+
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { BusquedaComponent } from './components/busqueda/busqueda.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    InicioComponent,
+    FooterComponent,
+    BusquedaComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ Front/src/app/models/ruta.ts
@@ -0,0 +1,9 @@
+export class Ruta{
+    constructor(
+        public _id:string,
+        public origen:string,
+        public destino:string,
+        public fechaSalida:string,
+        public fechaRetorno:string
+    ){}
+}
