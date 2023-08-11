b00742e6e6e5c7f48699de4397b78119af42ddb8
Eduardo Leines <jorge.leines@epn.edu.ec>
2023-08-11 12:38:08 -0500

    Cambios en busqueda, listado y services

--- Front/src/app/components/busqueda/busqueda.component.ts
+++ Front/src/app/components/busqueda/busqueda.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Global } from '../../services/global';
+import { OnInit, ViewChild } from '@angular/core';
+import { Ruta } from 'src/app/models/ruta';
+import { VueloService } from 'src/app/services/vuelo.service';
+
 
 @Component({
   selector: 'app-busqueda',
   templateUrl: './busqueda.component.html',
-  styleUrls: ['./busqueda.component.css']
+  styleUrls: ['./busqueda.component.css'],
+  providers:[VueloService]
 })
-export class BusquedaComponent {
+export class BusquedaComponent implements OnInit {
+  public ruta:Ruta;
+
+  constructor(
+    private _vueloservice: VueloService
+  ) { 
+    this.ruta = new Ruta("","","","","");
+  }
+
+  ngOnInit(): void {
+    
+  }
 
 }
--- Front/src/app/components/listado/listado.component.ts
+++ Front/src/app/components/listado/listado.component.ts
@@ -1,28 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { Ruta } from 'src/app/models/ruta';
+import { Vuelo } from 'src/app/models/vuelo';
 import { VueloService } from 'src/app/services/vuelo.service'; // Asegúrate de importar correctamente tu servicio
 
 @Component({
   selector: 'app-listado',
   templateUrl: './listado.component.html',
-  styleUrls: ['./listado.component.css']
+  styleUrls: ['./listado.component.css'],
+  providers: [VueloService]
 })
 export class ListadoComponent implements OnInit {
-  vuelos: any[] = []; // Aquí almacenaremos los datos de los vuelos
-
-  constructor(private vueloService: VueloService) { }
+  vuelos: Vuelo[]; // Aquí almacenaremos los datos de los vuelos
+  ruta: Ruta;
+  constructor(private vueloService: VueloService)
+  { 
+    this.ruta = new Ruta("","","","","");
+    this.vuelos = [];
+  }
 
   ngOnInit(): void {
     this.obtenerVuelos();
   }
 
   obtenerVuelos() {
-    const origen = 'Quito'; // Define los valores adecuados para origen, destino, fechaSalida y fechaRetorno
-    const destino = 'Cuenca';
-    const fechaSalida = '2023-08-15';
-    const fechaRetorno = '2023-09-13';
+    this.ruta.origen = 'Quito'; // Define los valores adecuados para origen, destino, fechaSalida y fechaRetorno
+    this.ruta.destino = 'Cuenca';
+    this.ruta.fechaSalida = '2023-08-15';
+    this.ruta.fechaRetorno = '2023-09-13';
 
 
-    this.vueloService.getVueloBusqueda(origen, destino, fechaSalida, fechaRetorno)
+    this.vueloService.getVueloBusqueda(this.ruta)
       .subscribe(
         (data) => {
           this.vuelos = data; // Asignamos los datos recibidos al arreglo de vuelos
