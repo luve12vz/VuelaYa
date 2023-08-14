d140d852e107ab712f1e9dc7c25f4a721c5d3ba7
Chafiiiiii <kevin.casagualpa@epn.edu.ec>
2023-08-13 21:56:46 -0500

    condiciones busqueda

--- Front/src/app/components/busqueda/busqueda.component.ts
+++ Front/src/app/components/busqueda/busqueda.component.ts
@@ -4,7 +4,7 @@ import { VueloService } from 'src/app/services/vuelo.service';
 import { Aeropuerto } from 'src/app/models/aeropuerto';
 import { RutaS } from 'src/app/models/rutaS';
 import { ActivatedRoute, Router } from '@angular/router';
-
+import { NgbDateStruct, NgbCalendar } from '@ng-bootstrap/ng-bootstrap';
 declare let $: any;
 
 @Component({
@@ -19,17 +19,22 @@ export class BusquedaComponent implements OnInit {
   public isDisabled: boolean = true;
   public isDisabledOption: boolean = true;
   public valorUbicacion: any = undefined;
- /* public vuelo:Vuelo;
-  constructor(
-    private 
-  )*/
+  public today: NgbDateStruct;
+  public maxDate: NgbDateStruct;
+  /* public vuelo:Vuelo;
+   constructor(
+     private 
+   )*/
   constructor(
     private _vueloservice: VueloService,
     private _router: Router,
-    private _route: ActivatedRoute
+    private _route: ActivatedRoute,
+    private calendar: NgbCalendar
   ) {
     this.rutaS = new RutaS("", "", "", "");
     this.aeropuertos = [];
+    this.today = this.calendar.getToday();
+    this.maxDate = { year: 2024, month: 12, day: 31 };
   }
 
   ngOnInit(): void {
@@ -41,16 +46,16 @@ export class BusquedaComponent implements OnInit {
   ngAfterViewInit(): void {
     this.validateOriginAndDestination();
   }
-/*   setupDatepicker() {
-    const today = new Date();
-    $("#datepicker").datepicker({
-      dateFormat: "dd-mm-yy",
-      changeMonth: true,
-      changeYear: true,
-      yearRange: "2023:2024",
-      minDate: today,
-    });
-  } */
+  /*   setupDatepicker() {
+      const today = new Date();
+      $("#datepicker").datepicker({
+        dateFormat: "dd-mm-yy",
+        changeMonth: true,
+        changeYear: true,
+        yearRange: "2023:2024",
+        minDate: today,
+      });
+    } */
   setupIncrementDecrement(): void {
     const decrementBtn = document.getElementById("decrementBtn") as HTMLButtonElement;
     const incrementBtn = document.getElementById("incrementBtn") as HTMLButtonElement;
@@ -68,71 +73,72 @@ export class BusquedaComponent implements OnInit {
       if (currentValue < 10) {
         quantityInput.value = (currentValue + 1).toString();
       }
-    });   
-}
-validateOriginAndDestination(): void {
-  const origenInput = document.getElementById("origen") as HTMLInputElement;
-  const destinoInput = document.getElementById("destino") as HTMLInputElement;
-
-  if (origenInput && destinoInput) {
-    origenInput.addEventListener("input", () => {
-      this.checkOriginAndDestination(origenInput, destinoInput);
-    });
-
-    destinoInput.addEventListener("input", () => {
-      this.checkOriginAndDestination(origenInput, destinoInput);
     });
   }
-}
+  validateOriginAndDestination(): void {
+    const origenInput = document.getElementById("origen") as HTMLInputElement;
+    const destinoInput = document.getElementById("destino") as HTMLInputElement;
 
-checkOriginAndDestination(originInput: HTMLInputElement, destinationInput: HTMLInputElement): void {
-  const origin = originInput.value;
-  const destination = destinationInput.value;
-  
-  if (origin === destination) {
-    window.alert("No se puede ingresar la misma cuidad de origen y destino");
-    originInput.value = ""; // Limpiar el campo de origen
-    destinationInput.value = ""; // Limpiar el campo de destino
-    // Puedes mostrar un mensaje de error, agregar clases de estilo, etc.
-  } else {
-    // Puedes limpiar mensajes de error, estilos, etc.
+    if (origenInput && destinoInput) {
+      origenInput.addEventListener("input", () => {
+        this.checkOriginAndDestination(origenInput, destinoInput);
+      });
+
+      destinoInput.addEventListener("input", () => {
+        this.checkOriginAndDestination(origenInput, destinoInput);
+      });
+    }
   }
-}
 
-getAeropuertos(){
-  this._vueloservice.getAeropuertos().subscribe(
-    response=> {
-      if (response.aeropuertos) {
-        this.aeropuertos = response.aeropuertos
-        console.log(this.aeropuertos)
-      }
+  checkOriginAndDestination(originInput: HTMLInputElement, destinationInput: HTMLInputElement): void {
+    const origin = originInput.value;
+    const destination = destinationInput.value;
+
+    if (origin === destination) {
+      window.alert("No se puede ingresar la misma cuidad de origen y destino");
+      originInput.value = ""; // Limpiar el campo de origen
+      destinationInput.value = ""; // Limpiar el campo de destino
+      // Puedes mostrar un mensaje de error, agregar clases de estilo, etc.
+    } else {
+      // Puedes limpiar mensajes de error, estilos, etc.
     }
-  )
-}
+  }
 
-checkDestino(vueloDestinoValue){
-  this.valorUbicacion = vueloDestinoValue;
-  console.log(vueloDestinoValue);
-  this.isDisabled = false;
-}
-
-getVueloRuta(){
-  this.rutaS.fechaSalida = this.rutaS.fechaSalida.year+"-"+('0'+this.rutaS.fechaSalida.month).slice(-2)
-  +"-"+('0'+this.rutaS.fechaSalida.day).slice(-2)
-  console.log(this.rutaS.fechaSalida);
-  this._vueloservice.getVueloBusquedaS(this.rutaS).subscribe(
-    response => {
-      if (response.rutasEncontradas){
-        this.rutaS = response.rutasEncontradas;
-        console.log(this.rutaS);
+  getAeropuertos() {
+    this._vueloservice.getAeropuertos().subscribe(
+      response => {
+        if (response.aeropuertos) {
+          this.aeropuertos = response.aeropuertos
+          console.log(this.aeropuertos)
+        }
       }
-    },
-    error => {
-      console.log(error);
-    },
-    () => { // Se envia a la pagina correspondiente
-      this._router.navigate(['/lista-vuelos', this.rutaS[0]._id])
-    }
-  )
-}
+    )
+  }
+
+  checkDestino(vueloDestinoValue) {
+    this.valorUbicacion = vueloDestinoValue;
+    console.log(vueloDestinoValue);
+    this.isDisabled = false;
+  }
+
+  getVueloRuta() {
+    this.rutaS.fechaSalida = this.rutaS.fechaSalida.year + "-" + ('0' + this.rutaS.fechaSalida.month).slice(-2)
+      + "-" + ('0' + this.rutaS.fechaSalida.day).slice(-2)
+    console.log(this.rutaS.fechaSalida);
+    this._vueloservice.getVueloBusquedaS(this.rutaS).subscribe(
+      response => {
+        if (response.rutasEncontradas) {
+          this.rutaS = response.rutasEncontradas;
+          console.log(this.rutaS);
+        }
+      },
+      error => {
+          console.log("No hay vuelos");
+          alert("No hay vuelos disponibles para esa fecha");
+      },
+      () => { // Se envia a la pagina correspondiente
+        this._router.navigate(['/lista-vuelos', this.rutaS[0]._id])
+      }
+    )
+  }
 }  
