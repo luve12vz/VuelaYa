c8eea2fe49404c2ca3d0b73c7a364a1de01f0db7
Ivonne-Ramirez <ivonne.ramirez@epn.edu.ec>
2023-08-15 00:12:58 -0500

    routas- redireccion del footer

--- Front/src/app/app-routing.module.ts
+++ Front/src/app/app-routing.module.ts
@@ -3,12 +3,18 @@ import { RouterModule, Routes } from '@angular/router';
 import { InicioComponent } from './components/inicio/inicio.component';
 import { ListadoComponent } from './components/listado/listado.component';
 import { ResumenDeVueloComponent } from './components/resumen-de-vuelo/resumen-de-vuelo.component';
+import { NosotrosComponent } from './components/nosotros/nosotros.component';
+import { BeneficiosComponent } from './components/beneficios/beneficios.component';
+import { RedComponent } from './components/red/red.component';
 
 const routes: Routes = [
-  {path:'home', component:InicioComponent},
-  {path:'',redirectTo:'home', pathMatch:'full'},
-  {path:'lista-vuelos/:id', component:ListadoComponent},
-  {path:'resumen/:id', component:ResumenDeVueloComponent}
+  { path: 'home', component: InicioComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'lista-vuelos/:id', component: ListadoComponent },
+  { path: 'resumen/:id', component: ResumenDeVueloComponent }, 
+  { path: 'nosotros', component: NosotrosComponent},
+  { path: 'beneficios', component: BeneficiosComponent},
+  { path: 'red', component: RedComponent}
 ];
 
 @NgModule({
