3cc96496c9fc3606012bdf20e63f39b3c1e5664c
Ivonne-Ramirez <ivonne.ramirez@epn.edu.ec>
2023-08-22 22:12:37 -0500

    cambios hechos

--- Front/src/app/app.module.ts
+++ Front/src/app/app.module.ts
@@ -22,8 +22,18 @@ import { VueloService } from './services/vuelo.service';
 import { ResumenCompraComponent } from './components/resumen-compra/resumen-compra.component';
 import { SeleccionAsientosIdaComponent } from './components/seleccion-asientos-ida/seleccion-asientos-ida.component';
 import { SeleccionAsientosRegresoComponent } from './components/seleccion-asientos-regreso/seleccion-asientos-regreso.component';
+<<<<<<< Updated upstream
 import { ResumendevueloidavueltaComponent } from './components/resumendevueloidavuelta/resumendevueloidavuelta.component';
+<<<<<<< HEAD
 import { SeleccionPasajerosComponent } from './components/seleccion-pasajeros/seleccion-pasajeros.component';
+=======
+import { IngresoDatosComponent } from './components/ingreso-datos/ingreso-datos.component';
+import { EquipajeExtraComponent } from './components/equipaje-extra/equipaje-extra.component';
+=======
+import { ListadoRegresoComponent } from './components/listado-regreso/listado-regreso.component';
+import { ResumenCompraIdaVueltaComponent } from './components/resumen-compra-ida-vuelta/resumen-compra-ida-vuelta.component';
+>>>>>>> Stashed changes
+>>>>>>> c615428b7611d742fd3d5cf5781d1d101eef4ab1
 
 @NgModule({
   declarations: [
@@ -40,8 +50,19 @@ import { SeleccionPasajerosComponent } from './components/seleccion-pasajeros/se
     ResumenCompraComponent,
     SeleccionAsientosIdaComponent,
     SeleccionAsientosRegresoComponent,
+<<<<<<< HEAD
     ResumendevueloidavueltaComponent,
     SeleccionPasajerosComponent
+=======
+<<<<<<< Updated upstream
+    ResumendevueloidavueltaComponent,
+    IngresoDatosComponent,
+    EquipajeExtraComponent
+=======
+    ListadoRegresoComponent,
+    ResumenCompraIdaVueltaComponent
+>>>>>>> Stashed changes
+>>>>>>> c615428b7611d742fd3d5cf5781d1d101eef4ab1
   ],
   imports: [
     BrowserModule,
