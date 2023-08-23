a18bcf08d0bd7f48b4d355b0aa0b04dcb0c789c8
Ivonne-Ramirez <ivonne.ramirez@epn.edu.ec>
2023-08-22 22:15:56 -0500

    hecho

--- Front/src/app/app.module.ts
+++ Front/src/app/app.module.ts
@@ -22,18 +22,16 @@ import { VueloService } from './services/vuelo.service';
 import { ResumenCompraComponent } from './components/resumen-compra/resumen-compra.component';
 import { SeleccionAsientosIdaComponent } from './components/seleccion-asientos-ida/seleccion-asientos-ida.component';
 import { SeleccionAsientosRegresoComponent } from './components/seleccion-asientos-regreso/seleccion-asientos-regreso.component';
-<<<<<<< Updated upstream
+
 import { ResumendevueloidavueltaComponent } from './components/resumendevueloidavuelta/resumendevueloidavuelta.component';
-<<<<<<< HEAD
 import { SeleccionPasajerosComponent } from './components/seleccion-pasajeros/seleccion-pasajeros.component';
-=======
+
 import { IngresoDatosComponent } from './components/ingreso-datos/ingreso-datos.component';
 import { EquipajeExtraComponent } from './components/equipaje-extra/equipaje-extra.component';
-=======
+
 import { ListadoRegresoComponent } from './components/listado-regreso/listado-regreso.component';
 import { ResumenCompraIdaVueltaComponent } from './components/resumen-compra-ida-vuelta/resumen-compra-ida-vuelta.component';
->>>>>>> Stashed changes
->>>>>>> c615428b7611d742fd3d5cf5781d1d101eef4ab1
+
 
 @NgModule({
   declarations: [
@@ -50,19 +48,12 @@ import { ResumenCompraIdaVueltaComponent } from './components/resumen-compra-ida
     ResumenCompraComponent,
     SeleccionAsientosIdaComponent,
     SeleccionAsientosRegresoComponent,
-<<<<<<< HEAD
+
     ResumendevueloidavueltaComponent,
-    SeleccionPasajerosComponent
-=======
-<<<<<<< Updated upstream
-    ResumendevueloidavueltaComponent,
-    IngresoDatosComponent,
-    EquipajeExtraComponent
-=======
+    SeleccionPasajerosComponent,
     ListadoRegresoComponent,
     ResumenCompraIdaVueltaComponent
->>>>>>> Stashed changes
->>>>>>> c615428b7611d742fd3d5cf5781d1d101eef4ab1
+
   ],
   imports: [
     BrowserModule,
