9b56e87184d19c42efdde8ffd1e45e58840ce87b
Ivonne-Ramirez <ivonne.ramirez@epn.edu.ec>
2023-08-22 22:31:32 -0500

    actualizacion rutas

--- Front/src/app/app-routing.module.ts
+++ Front/src/app/app-routing.module.ts
@@ -10,19 +10,27 @@ import { ResumenCompraComponent } from './components/resumen-compra/resumen-comp
 import { SeleccionAsientosIdaComponent } from './components/seleccion-asientos-ida/seleccion-asientos-ida.component';
 import { SeleccionAsientosRegresoComponent } from './components/seleccion-asientos-regreso/seleccion-asientos-regreso.component';
 import { ResumendevueloidavueltaComponent } from './components/resumendevueloidavuelta/resumendevueloidavuelta.component';
+import { ResumenCompraIdaVueltaComponent } from './components/resumen-compra-ida-vuelta/resumen-compra-ida-vuelta.component';
+import { EquipajeExtraComponent } from './components/equipaje-extra/equipaje-extra.component';
+import { IngresoDatosComponent } from './components/ingreso-datos/ingreso-datos.component';
+import { ListadoRegresoComponent } from './components/listado-regreso/listado-regreso.component';
 
 const routes: Routes = [
   { path: 'home', component: InicioComponent },
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'lista-vuelos/:id/:p', component: ListadoComponent },
-  { path: 'resumen/:id/:p', component: ResumenDeVueloComponent }, 
-  { path: 'nosotros', component: NosotrosComponent},
-  { path: 'beneficios', component: BeneficiosComponent},
-  { path: 'red', component: RedComponent},
-  {path: 'seleccion_asientos_ida',component:SeleccionAsientosIdaComponent},
-  {path:'seleccion_asientos_regreso',component:SeleccionAsientosRegresoComponent},
-  { path: 'compra/:id/:p', component:ResumenCompraComponent },
-  { path: 'resumendevueloidavuelta/:id/:p', component:ResumendevueloidavueltaComponent }
+  { path: 'resumen/:id/:p', component: ResumenDeVueloComponent },
+  { path: 'nosotros', component: NosotrosComponent },
+  { path: 'beneficios', component: BeneficiosComponent },
+  { path: 'red', component: RedComponent },
+  { path: 'seleccion_asientos_ida', component: SeleccionAsientosIdaComponent },
+  { path: 'seleccion_asientos_regreso', component: SeleccionAsientosRegresoComponent },
+  { path: 'compra/:id/:p', component: ResumenCompraComponent },
+  { path: 'resumendevueloidavuelta/:id/:p', component: ResumendevueloidavueltaComponent },
+  { path: 'resumen-compra-ida-vuelta/:id/:p', component: ResumenCompraIdaVueltaComponent },
+  { path: 'equipaje-extra', component: EquipajeExtraComponent },
+  { path: 'ingreso-datos', component: IngresoDatosComponent },
+  { path: 'listado-regreso', component: ListadoRegresoComponent }
 ];
 
 @NgModule({
