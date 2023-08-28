20e92dda32c021bc353289a6907cf8a3dfe77496
Ivonne-Ramirez <ivonne.ramirez@epn.edu.ec>
2023-08-28 12:27:40 -0500

    diseño de equipaje

--- Front/src/app/components/equipaje-extra/equipaje-extra.component.ts
+++ Front/src/app/components/equipaje-extra/equipaje-extra.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
+
 @Component({
   selector: 'app-equipaje-extra',
   templateUrl: './equipaje-extra.component.html',
