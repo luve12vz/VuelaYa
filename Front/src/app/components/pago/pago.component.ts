cb829f22d4ac6b1eca5897342b27e758866d6d33
Chafiiiiii <kevin.casagualpa@epn.edu.ec>
2023-09-04 22:42:31 -0500

    actulizacion de pagos

--- Front/src/app/components/pago/pago.component.ts
+++ Front/src/app/components/pago/pago.component.ts
@@ -26,8 +26,12 @@ export class PagoComponent implements OnInit{
   public showError: boolean = false;
   public params: any;
 
-  total = 0;
-
+  public total = 0;
+  public totalida = 0;
+  public totalregreso=0;
+  public asientosTotales: any[] = []; 
+  public valormaletas=0;
+  public descuento=0;
   @ViewChild('priceElem', { static: false }) priceElem?: ElementRef;
   esIV: any;
   pasajeros: any;
@@ -96,7 +100,73 @@ export class PagoComponent implements OnInit{
       }
     )
   }
+  //muestra en un array la cantidad total de los asientos 
+  calcularAsientosTotales() {
+    let asientosTotales: any[] = [...this.asientosI];
 
+    // Si es un vuelo de ida y vuelta, combina los asientos de ida y vuelta en un solo arreglo
+    if (this.esIV === "R") {
+      asientosTotales = [...asientosTotales, ...this.asientosR];
+    }
+
+    // Filtra los asientos ocupados (eliminando espacios vacíos y comas duplicadas)
+    asientosTotales = asientosTotales.filter(asiento => asiento.trim() !== '');
+
+    // Limpia los valores de los asientos utilizando una expresión regular
+    asientosTotales = asientosTotales.map(asiento => asiento.replace(/,/g, ''));
+
+    // Une los asientos en una cadena separada por comas
+    const asientosOcupados = ',' + asientosTotales.join(',') + ',';
+
+    return asientosOcupados;
+}
+  caluloMaletas(){
+    const e23 = parseInt(this.e23);
+    const em =parseInt(this.em);
+    return this.valormaletas=(e23+em)*65;
+  }
+  CalculoPagoAsientos(){
+    this.pasajeros = JSON.parse(this.params.p);
+    const pasajerosNumero = parseInt(this.pasajeros[0]);
+    const pasajerosNumero1 = parseInt(this.pasajeros[1]);
+    const pasajerosNumero2 = parseInt(this.pasajeros[2]);
+    const pasajerosNumero3 = parseInt(this.pasajeros[3]);
+
+    var totalpasajeros=this.pasajeros[0]+this.pasajeros[1]+this.pasajeros[2]+this.pasajeros[3];
+   
+    this.totalida=this.vueloI.precio*(totalpasajeros);
+    this.totalregreso=this.vueloR.precio*(totalpasajeros);
+    return this.totalregreso+this.totalida; 
+  }
+  calculoValoradd(){
+    if (this.esIV === "I") {
+      this.idVI = this.params.idVI;
+      this.asientosI = JSON.parse(this.params.aI); // Convierte a un arreglo
+    } else {
+      this.idVI = this.params.idVI;
+      this.idVR = this.params.idVR;
+      this.asientosI = JSON.parse(this.params.aI); // Convierte a un arreglo
+      this.asientosR = JSON.parse(this.params.aR); // Convierte a un arreglo
+    }
+    
+    return this.asientosI[2]
+  }
+  calculoDescuento(){
+    this.pasajeros = JSON.parse(this.params.p);
+    const pasajerosNumero = parseInt(this.pasajeros[0]);
+    const pasajerosNumero3 = parseInt(this.pasajeros[3]);
+    if(pasajerosNumero>0){
+      this.descuento=this.descuento+((this.vueloI.precio+this.vueloR.precio)/2);
+    }if(pasajerosNumero3>0){
+      this.descuento=this.descuento+this.vueloI.precio+this.vueloR.precio;
+    }else{
+      this.descuento=0;
+    }
+    return this.descuento
+  }
+  totalpago(){
+    return this.CalculoPagoAsientos()+this.caluloMaletas()-this.calculoDescuento();
+  }
  /* updateTotal() {
     this.cart.forEach((cartItem) => {
       this.items.push({
